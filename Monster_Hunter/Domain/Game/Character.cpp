6513e930700be808837922bffbefa1fa714a4972
Kasim <panjrikasim@gmail.com>
2019-10-23 16:24:47 -0700

    Updated Character class

--- Monster_Hunter/Domain/Game/Character.cpp
+++ Monster_Hunter/Domain/Game/Character.cpp
@@ -1,12 +1,108 @@
 #include "pch.h"
-#include "Character.h"
-
+#include "Character.hpp"
 
 Character::Character()
 {
+	name = "NA";
+	weapon_name = "NA";
+	attributes = 1;
+	max_health = 50;
+	current_health = max_health;
+	potion = 3;
+	damage = 1;
+	weapon_attributes = 1;
+}
+
+void Character::loadvars()
+{
+	//placeholder code for image
+	for (int i = 0; i < 20; ++i)
+	{
+		for (int j = 0; j < 20; ++j)
+		{
+			image[i][j] = std::to_string(i + j);
+		}
+
+	}
+
+
+}
+
+void Character::gain_Attribute(int value)
+{
+	attributes += value;
+}
+
+void Character::equip_Weapon(int wa, std::string wn)
+{
+	weapon_attributes = wa;
+	weapon_name = wn;
+	attributes += weapon_attributes;
+}
+
+bool Character::use_Potion()
+{
+	if (potion > 0)
+	{
+		potion -= 1;
+		return true;
+	}
+	return false;
 }
 
 
-Character::~Character()
+void Character::heal_using_Campfires()
 {
+	int i = 0;
+	while (current_health < max_health)
+	{
+		current_health += max_health * 10;
+		Thread.sleep(1000);
+	}
+}
+
+std::string** Character::get_image()
+{
+	return image;
+}
+
+int Character::get_attributes()
+{
+	return 0;
+}
+
+void Character::set_attributes(const int x)
+{
+}
+
+int Character::get_health()
+{
+	return 0;
+}
+
+void Character::set_health(const int x)
+{
+}
+
+int Character::get_potion()
+{
+	return 0;
+}
+
+void Character::set_potion(const int x)
+{
+}
+
+int Character::get_damage()
+{
+	return 0;
+}
+
+void Character::set_damage(const int x)
+{
+}
+
+int Character::get_weapon_attributes()
+{
+	return 0;
 }
--- Monster_Hunter/Domain/Game/Character.hpp
+++ Monster_Hunter/Domain/Game/Character.hpp
@@ -1,8 +1,37 @@
 #pragma once
+#include <iostream>
+#include <string>
+
 class Character
 {
+protected:
+	int attributes;
+	int max_health;
+	int current_health;
+	int potion;
+	int damage;
+	int weapon_attributes;
+	std::string name;
+	std::string image[20][20];
+	std::string weapon_name;
 public:
 	Character();
-	~Character();
+	~Character() = default;
+	virtual void loadimage();
+	void gain_Attribute(int value, int value_health);
+	void equip_Weapon(int wa, std::string wn);
+	bool use_Potion();
+	virtual void heal_using_Campfires();
+	
+	int get_attributes();
+	void set_attributes(const int x);
+	int get_health();
+	void set_health(const int x);
+	int get_potion();
+	void set_potion(const int x);
+	int get_damage();
+	void set_damage(const int x);
+	int get_weapon_attributes();
+	std::string** get_image();
 };
 
