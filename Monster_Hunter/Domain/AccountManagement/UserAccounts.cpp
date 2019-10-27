6bddd4254e8c2c76a91ad80640f7bfb6698cb1b3
Kasim <panjrikasim@gmail.com>
2019-10-26 17:10:34 -0700

    Changed Accountmanagement class to AccountmanagementHandler

--- Monster_Hunter/Domain/AccountManagement/UserAccounts.cpp
+++ Monster_Hunter/Domain/AccountManagement/UserAccounts.cpp
@@ -1,6 +1,8 @@
-//#include "pch.h"
-#include "UserAccounts.hpp"
+#include <algorithm>
 
+#include "UserAccounts.hpp"
+#include "../../TechnicalServices/Logging/SimpleLogger.hpp"
+#include "../../TechnicalServices/Persistence/SimpleDB.hpp"
 
 UserAccounts::UserAccounts()
 {
--- Monster_Hunter/Domain/AccountManagement/UserAccounts.hpp
+++ Monster_Hunter/Domain/AccountManagement/UserAccounts.hpp
@@ -1,8 +1,25 @@
 #pragma once
-class UserAccounts
-{
-public:
-	UserAccounts();
-	~UserAccounts();
-};
+#include <memory>
+#include <string>
 
+#include "AccountManagementHandler.hpp"
+#include "../../TechnicalServices/Logging/LoggerHandler.hpp"
+#include "../../TechnicalServices/Persistence/PersistenceHandler.hpp"
+
+namespace Domain::AccountManagement
+{
+	class UserAccounts :public AccountManagementHandler
+	{
+	public:
+		using AccountManagementHandler::AccountManagementHandler;
+		UserAccounts();
+		bool isAuthenticated(const UserCredentials & credentials) override;
+		~UserAccounts() noexcept override;
+
+	private:
+		std::unique_ptr<TechnicalServices::Persistence::PersistenceHandler> _perssistentData;
+		std::unique_ptr <TechnicalServices::Logging::LoggerHandler> _loggerPtr;
+
+		TechnicalServices::Logging::LoggerHandler & _logger = *_loggerPtr;
+	};
+}
